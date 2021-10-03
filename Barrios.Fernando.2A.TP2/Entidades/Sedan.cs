c001cb819331ca439f3bda22ec109bb956e6efc6
fgbarrios <78186226+fgbarrios@users.noreply.github.com>
2021-10-03 18:20:05 -0300

    TP 2

---
+++ Barrios.Fernando.2A.TP2/Entidades/Sedan.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Sedan : Vehiculo
+    {
+        #region "Enumerados"
+        public enum ETipo 
+        { 
+            CuatroPuertas, 
+            CincoPuertas 
+        }
+
+        #endregion
+
+        #region "Atributos"
+
+        private ETipo tipo;
+
+        #endregion
+
+        #region "Propiedades"
+        /// <summary>
+        /// Sedan son 'Mediano'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+
+        #endregion
+
+        #region "Constructores"
+
+        /// <summary>
+        /// Por defecto, TIPO será CuatroPuertas
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color)
+            : base(marca, chasis, color)
+        {
+            tipo = ETipo.CuatroPuertas;
+        }
+
+        public Sedan(EMarca marca, string chasis, ConsoleColor color, ETipo tipo)
+            :base(marca, chasis, color)
+        {
+            this.tipo = tipo;
+        }
+
+        #endregion
+
+        #region "Sobrecargas"
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SEDAN");
+            sb.AppendLine(base.Mostrar());
+
+            sb.AppendFormat("TIPO : {0} ", this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ Barrios.Fernando.2A.TP2/Entidades/Suv.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Suv: Vehiculo
+    {
+        #region "Constructores"
+        public Suv(EMarca marca, string chasis, ConsoleColor color)
+            : base( marca, chasis, color)
+        {
+        }
+
+        #endregion
+
+        #region "Propiedades"
+        /// <summary>
+        /// SUV son 'Grande'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+
+        #endregion
+
+        #region "Sobrecargas"
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SUV");
+            sb.AppendLine(base.Mostrar());
+
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        #endregion
+    }
+}
