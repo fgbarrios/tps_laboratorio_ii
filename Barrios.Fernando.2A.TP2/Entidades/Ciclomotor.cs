6961ad1d87c8a0887d12e49bab476c8c4a78aa06
fgbarrios <78186226+fgbarrios@users.noreply.github.com>
2021-10-03 18:08:20 -0300

    TP 2

---
+++ Barrios.Fernando.2A.TP2/Entidades/Ciclomotor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        #region "Constructores"
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color)
+            :base(marca, chasis, color)
+        {
+        }
+        #endregion
+
+        #region "Propiedades"
+
+        /// <summary>
+        /// Ciclomotor son 'Chico'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Chico;
+            }
+        }
+
+        #endregion
+
+        #region "Sobrecargas"
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendLine(base.Mostrar());
+
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        #endregion
+    }
+}
